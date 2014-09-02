0063ab1407cde9d824d49bfcf994cdd45b47505a
Chao <imchaozhang@gmail.com>
2014-09-02 13:19:05 -0700

    add "{}" and "$ALL" to the mongo_query plugin output pattern, so now the query can project the whole json file of the finded documents.

--- src/MongoQueryAtom.cpp
+++ src/MongoQueryAtom.cpp
@@ -48,93 +48,110 @@ namespace dlvhex {
         void
         MongoQueryAtom::retrieve(const Query& query, Answer& answer) throw (PluginError) {
             Registry &registry = *getRegistry();
-            
+
             mongo::DBClientConnection c;
             ConfigFile cfg("include/config.cfg");
             std::string connection = cfg.getValueOfKey("CONNECTION_STRING");
-            
+
 
             // try the connection of the mongo db at first               
             try {
-                
-                 c.connect(connection);
-                
-                } catch (const mongo::DBException &e) {
-                	throw PluginError("MongoDB is not on, or connection breaks!");
+
+                c.connect(connection);
+
+            } catch (const mongo::DBException &e) {
+                throw PluginError("MongoDB is not on, or connection breaks!");
             }
+
+
+            const Term& dbc = registry.terms.getByID(query.input[0]);
+            const Term& jpattern = registry.terms.getByID(query.input[1]);
+            const Term& output = registry.terms.getByID(query.input[2]);
+
+
+
+            if (!dbc.isQuotedString()) {
+                throw PluginError("Wrong input argument type for input 1, need to be string");
+            }
+
+
+            if (!jpattern.isQuotedString()) {
+                throw PluginError("Wrong input argument type for input 2, need to be string");
+            }
+
+            if (!output.isQuotedString()) {
+                throw PluginError("Wrong input argument type for input 3, need to be string");
+            }
+
+
+            std::string s_dbc = dbc.getUnquotedString();
+            std::string s_jpattern = jpattern.getUnquotedString();
+            std::string s_output = output.getUnquotedString();
+
+
+            if (s_output == "") {
+
+                throw PluginError("The output pattern cannot be empty");
+
+            }
+
+            // The ouput projection items shuld be seperated by ; 
+            std::vector<string> output_v;
+            boost::split(output_v, s_output, boost::is_any_of(";"));
+
+            // Start up a cursor to run a tranverse search for the data
+            std::auto_ptr<DBClientCursor> cursor = c.query(s_dbc, fromjson(s_jpattern));
+
+            if (!cursor.get()) {
+
+                throw PluginError("query failure");
+
+            }
+
+            while (cursor->more()) {
+
+                Tuple out;
+
+                BSONObj p = cursor->next();
+
+                if (s_output == "{}" || s_output == "$ALL") {
                 
+		        std::string in;
+		        
+		        in = p.toString();
+		        
+		        boost::replace_all(in, "\"", "'");
+		                        if (in.substr(0, 1) == "'" && in.substr(in.size() - 1, 1) == "'") {
 
-                const Term& dbc = registry.terms.getByID(query.input[0]);
-                const Term& jpattern = registry.terms.getByID(query.input[1]);
-                const Term& output = registry.terms.getByID(query.input[2]);
+		                            boost::replace_all(in, "'", "");
+		                        }
 
-
-                
-                if (!dbc.isQuotedString()) {
-                    throw PluginError("Wrong input argument type for input 1, need to be string");
-                }
-
-
-                if (!jpattern.isQuotedString()) {
-                    throw PluginError("Wrong input argument type for input 2, need to be string");
-                }
-
-                if (!output.isQuotedString()) {
-                    throw PluginError("Wrong input argument type for input 3, need to be string");
-                }
+		        Term term(ID::MAINKIND_TERM | ID::SUBKIND_TERM_CONSTANT, '"' + in + '"');
+		                    out.push_back(registry.storeTerm(term));
 
 
-                std::string s_dbc = dbc.getUnquotedString();
-                std::string s_jpattern = jpattern.getUnquotedString();
-                std::string s_output = output.getUnquotedString();
+                } else {
 
-             
-                    if (s_output == "") {
+                    for (size_t i = 0; i < output_v.size(); i++) {
 
-                        throw PluginError("The output pattern cannot be empty");
+                        std::string in;
 
-                    }
-                    
-                    // The ouput projection items shuld be seperated by ; 
-                    std::vector<string> output_v;
-                    boost::split(output_v, s_output, boost::is_any_of(";"));
-                    
-                    // Start up a cursor to run a tranverse search for the data
-                    std::auto_ptr<DBClientCursor> cursor = c.query(s_dbc, fromjson(s_jpattern));
+                        try {
 
-                    if (!cursor.get()) {
+                            in = p.getFieldDotted(output_v[i]).String();
 
-                        throw PluginError("query failure");
-
-                    }
-
-                    while (cursor->more()) {
-
-                        Tuple out;
-                        
-                        BSONObj p = cursor->next();
-
-                        for (size_t i = 0; i < output_v.size(); i++) {
-                            
-                            std::string in;
-                            
-                            try {
-
-                                    in = p.getFieldDotted(output_v[i]).String();
-
-                                } catch (DBException &e) {
+                        } catch (DBException &e) {
 
                             if (p.getFieldDotted(output_v[i]).isNumber()) {
-                                
+
                                 ostringstream convert;
                                 convert << p.getFieldDotted(output_v[i]).toString(false);
                                 in = convert.str();
                                 // if it is int, remove the last two digits
-                                if(in.size()>=3 && in.substr(in.size()-2,2) == ".0")
-                                in = in.substr(0, in.size()-2);
+                                if (in.size() >= 3 && in.substr(in.size() - 2, 2) == ".0")
+                                    in = in.substr(0, in.size() - 2);
 
-                            } 
-                            else if (p.getFieldDotted(output_v[i]).isBoolean()) {
+                            } else if (p.getFieldDotted(output_v[i]).isBoolean()) {
                                 ostringstream convert;
 
                                 if (p.getFieldDotted(output_v[i]).boolean() == true)
@@ -142,73 +159,67 @@ namespace dlvhex {
                                 else
                                     in = "false";
 
-                            }
-                            
-                            // Check if the element type is Date
-                            else if(p.getFieldDotted(output_v[i]).type() == 9){
+                            }// Check if the element type is Date
+                            else if (p.getFieldDotted(output_v[i]).type() == 9) {
 
                                 ostringstream convert;
-                                
-        			convert << p.getFieldDotted(output_v[i]).date();
-        			
-        		
-        			in = convert.str();
-        			
-        			if(in.size() == 13)
-        			 // remove millionsecond to the epoch time stamp
-        			 in = in.substr(0,10);
-        			
 
-       				}
-                            
-                            else if(!p.getFieldDotted(output_v[i]).eoo()){
+                                convert << p.getFieldDotted(output_v[i]).date();
 
-                                
-        			in = p.getFieldDotted(output_v[i]).toString(false);
-        			
-        			//in.erase(
-                    		//	remove(in.begin(), in.end(), '\"'),
-                   		// 	in.end()
-                    		//        );    
-       				boost::replace_all(in,"\"","'");       				
-                    		if(in.substr(0,1)=="'" && in.substr(in.size()-1,1) == "'"){
-                    		
-                       			boost::replace_all(in,"'",""); 
-                    		}         				    				
-       				
-       				}
-                            
-                            else {
-                            
-                            	in = "";
-                            
-                            	}
-                            	
-                            }
-                            
-                            //if(!p.hasField(output_v[i]))
-                            // {
-                            //	cout<< "Some of the document do not have field \"" << output_v[i] << "\"" << endl;
-                            //}
 
-                            try {
-                                int v;
-                                v = boost::lexical_cast<int>(in);
-                                Term term(ID::MAINKIND_TERM | ID::SUBKIND_TERM_CONSTANT, in);
-                                out.push_back(registry.storeTerm(term));
+                                in = convert.str();
 
-                            } catch (boost::bad_lexical_cast& e) {
+                                if (in.size() == 13)
+                                    // remove millionsecond to the epoch time stamp
+                                    in = in.substr(0, 10);
 
-                                Term term(ID::MAINKIND_TERM | ID::SUBKIND_TERM_CONSTANT, '"' + in + '"');
-                                out.push_back(registry.storeTerm(term));
+
+                            } else if (!p.getFieldDotted(output_v[i]).eoo()) {
+
+
+                                in = p.getFieldDotted(output_v[i]).toString(false);
+
+                                //in.erase(
+                                //	remove(in.begin(), in.end(), '\"'),
+                                // 	in.end()
+                                //        );    
+                                boost::replace_all(in, "\"", "'");
+                                if (in.substr(0, 1) == "'" && in.substr(in.size() - 1, 1) == "'") {
+
+                                    boost::replace_all(in, "'", "");
+                                }
+
+                            } else {
+
+                                in = "";
+
                             }
 
+                        } // end of try - catch
+
+                        //if(!p.hasField(output_v[i]))
+                        // {
+                        //	cout<< "Some of the document do not have field \"" << output_v[i] << "\"" << endl;
+                        //}
+
+                        try {
+                            int v;
+                            v = boost::lexical_cast<int>(in);
+                            Term term(ID::MAINKIND_TERM | ID::SUBKIND_TERM_CONSTANT, in);
+                            out.push_back(registry.storeTerm(term));
+
+                        } catch (boost::bad_lexical_cast& e) {
+
+                            Term term(ID::MAINKIND_TERM | ID::SUBKIND_TERM_CONSTANT, '"' + in + '"');
+                            out.push_back(registry.storeTerm(term));
                         }
-                        answer.get().push_back(out);
-                    }
+                    } // end of for
+                } // end of if - else
+                answer.get().push_back(out);
+            }// end of while
 
 
-            	
+
         } // end of retrieve function
 
     } // end of mongo
